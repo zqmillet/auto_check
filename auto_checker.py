8036bd6e94cb3eacce813c899e575d27c60a9b44
zqmillet <zqmillet@qq.com>
2018-12-11 14:59:22 +0000

    modify the file auto_checher.py

--- auto_checker.py
+++ auto_checker.py
@@ -1,6 +1,7 @@
 import functools
 import inspect
 import collections
+import ast
 
 from exceptions import InvalidValueError, InvalidCheckerError
 
@@ -54,7 +55,16 @@ def check(name, value, checker, function):
             raise InvalidCheckerError(name, function)
         return result
 
-def testcases():
+def execute(code):
+    block = ast.parse(code, mode = 'exec')
+    last = ast.Expression(block.body.pop().value)
+
+    _globals, _locals = {}. {}
+    exec(compile(block, '<string>', mode = 'exec'), _globals, _locals)
+    return eval(compile(last, '<string>', mode = 'eval'), _globals, _locals)
+
+
+def testcase():
     @auto_type_checker
     def add(a, b, c: (int, float), d: int) -> str:
         return a + b + c + d
